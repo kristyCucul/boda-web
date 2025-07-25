76f0e6f46cb1aa619354f0d99420d5d01716f3d1
kristyCucul <13.kristycuculg@gmail.com>
2025-07-24 22:22:46 -0900

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,18 +1,19 @@
 <?php
-$host = 'db.qokxguweyxmgwylvtcgv.supabase.co'; // Solo el host, sin "postgresql://"
+$host = 'db.qokxguweyxmgwylvtcgv.supabase.co';
 $port = '5432';
-$dbname = 'postgres'; // nombre de la base de datos
-$user = 'postgres';  // usuario correcto
-$password = 'tssped131328'; // tu contraseña
+$dbname = 'postgres';
+$user = 'postgres';
+$password = 'tssped131328';
 
 try {
   $conexion = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
   $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Conexión exitosa a Supabase PostgreSQL.";
+  // echo "Conexión exitosa a Supabase PostgreSQL.";
 } catch (PDOException $e) {
   echo "Error de conexión: " . $e->getMessage();
 }
 ?>
+
 
 
 
