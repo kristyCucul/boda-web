9ec937dfadfff288be6b9a87e4af3b3f5c4d3625
kristyCucul <13.kristycuculg@gmail.com>
2025-07-26 11:54:49 -0900

    Update wishlist.php

--- wishlist.php
+++ wishlist.php
@@ -333,7 +333,7 @@
 
     <div class="regalo">
       <img src="multimedia/wishlist/32.jpg" alt="Licuadora">
-      <h3>olino de Café</h3>
+      <h3>Molino de Café</h3>
     <a href="https://docs.google.com/forms/d/e/1FAIpQLSebMnUVtg6beGkNrygig353NguHOJXmfj8ooGDvK2H1jt2qrA/viewform?usp=header" target="_blank">
    <button type="button">Apartar</button>
     </a>
