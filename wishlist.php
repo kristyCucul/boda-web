b076ac59001c9e82a4525f7bd8ca3408af6f74ca
kristyCucul <13.kristycuculg@gmail.com>
2025-07-28 11:33:39 -0900

    Update wishlist.php

--- wishlist.php
+++ wishlist.php
@@ -135,8 +135,7 @@
     <div class="regalo">
       <img src="multimedia/wishlist/7.jpg" alt="Licuadora">
       <h3>Olla de Cocimiento Lento</h3>
-    <a href="https://docs.google.com/forms/d/e/1FAIpQLSebMnUVtg6beGkNrygig353NguHOJXmfj8ooGDvK2H1jt2qrA/viewform?usp=header" target="_blank">
-   <button type="button">Apartar</button>
+     <button type="button">✔ Apartado</button>
     </a>
     </div>
 
